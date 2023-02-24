5f91ba7af0209b54e3cc8c769eef5c470701362a
Maxim Burgerhout <maxim@wzzrd.com>
2023-02-24 19:54:41 +0100

    return apiVersion instead of string

--- src/create_api_version.ts
+++ src/create_api_version.ts
@@ -1,16 +1,16 @@
 import { gql, GraphQLClient } from 'graphql-request'
 import { UnexpectedResponseError } from './errors'
-import { createApiVersionResponseObject } from './types'
+import { apiVersion, createApiVersionResponseObject } from './types'
 
 /**
  * Creates and returns a new API version for a given API
  * @param {string} name Version name or number for the new API version
- * @param {*} apiId The id of the API to create a new version for
+ * @param {string} apiId The id of the API to create a new version for
  * @param {GraphQLClient} client The GraphQL Client object for reuse
- * @returns {Promise<string>} The id of the newly created API version
+ * @returns {Promise<apiVersion>} The id of the newly created API version
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-async function createApiVersion (name: string, apiId: string, client: GraphQLClient): Promise<string> {
+async function createApiVersion (name: string, apiId: string, client: GraphQLClient): Promise<apiVersion> {
   const mutation = gql`
     mutation createApiVersions($apiVersions: [ApiVersionCreateInput!]!) {
         createApiVersions(apiVersions: $apiVersions) {
@@ -31,7 +31,7 @@ async function createApiVersion (name: string, apiId: string, client: GraphQLCli
 
   try {
     const res: createApiVersionResponseObject = await client.request(mutation, params)
-    return res.createApiVersions[0].id
+    return res.createApiVersions[0]
   } catch (e) {
     console.log(e.response.errors)
     throw new UnexpectedResponseError('Unknown error in create_api_version')
