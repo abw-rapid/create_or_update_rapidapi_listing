c3a109ba690aaf7112356c1a55e8b832f8390ae2
Maxim Burgerhout <maxim@wzzrd.com>
2023-03-03 09:44:02 +0100

    move reading config file to own function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import * as toml from 'toml'
-import * as fs from 'fs'
 import * as core from '@actions/core'
 import * as g from 'graphql-request'
 import dotenv from 'dotenv'
@@ -17,18 +15,13 @@ import { getUpdateLevel } from './getUpdateLevel'
 import { updateApiVersion } from './update_api_version'
 import { getUpdatePolicy, handleUpdate } from './handle_update'
 import { apiPolicy, apiVersion } from './types'
+import { readConfig } from './read_config'
 
 
 (async function () {
 
     dotenv.config()
-
-    let config
-    try {
-        config = toml.parse(fs.readFileSync('./rapidConfig.default.toml', 'utf-8'))
-    } catch {
-        throw new Error('Unable to open default configuration file rapidConfig.default.toml')
-    }
+    const config = readConfig()
 
     const specPath = core.getInput('SPEC_PATH', { required: true })
     const graphqlUrl = core.getInput('GRAPHQL_URL', { required: true })
